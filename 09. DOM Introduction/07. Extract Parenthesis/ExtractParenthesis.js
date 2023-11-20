51dd17110aa47fe98b389a034db128d5a70a98f3
kristianche <chelebiev777@gmail.com>
2023-11-20 22:56:32 +0200

    Edited two of the tasks!

--- 09. DOM Introduction/07. Extract Parenthesis/ExtractParenthesis.js
+++ 09. DOM Introduction/07. Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,7 @@
 function extract(content) {
+    const text = document.getElementById('content').textContent;
+    const regex = /\(([\w\s]+)\)/g;
+    const matches = text.match(regex)
 
+    return matches.join('; ')
 }
